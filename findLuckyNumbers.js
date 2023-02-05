df3ca9b46dc29f3182f4af7721a348e11f017e66
Keldon-Lopez <keldon.lopz@gmail.com>
2023-02-04 17:30:21 -0700

    finished challenge

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,14 @@
 // Write your code below this line.
+const luckyNumbers = num => {
+    let arr = [];
+    for (let index = 0; arr.length < num; index++) {
+        let randomNum = Math.round(Math.random()*num);
+        console.log(randomNum);
+       if (!arr.includes(randomNum)) {
+               arr.push(randomNum);
+       } 
+       console.log(arr);
+    }
+    return arr;
+}
+luckyNumbers(7);
