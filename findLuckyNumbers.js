62b0c40bd9cecf2468c141c2374cb66f8152b7c4
Keldon-Lopez <keldon.lopz@gmail.com>
2023-02-04 17:34:10 -0700

    removed console logs

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -3,11 +3,9 @@ const luckyNumbers = num => {
     let arr = [];
     for (let index = 0; arr.length < num; index++) {
         let randomNum = Math.round(Math.random()*num);
-        console.log(randomNum);
        if (!arr.includes(randomNum)) {
                arr.push(randomNum);
        } 
-       console.log(arr);
     }
     return arr;
 }
